82334d52862aaa5389101fa38c3839a4e75ef866
Viet Vu <jooservices@gmail.com>
2015-04-20 15:27:01 +0700

    ^ update factory to render html

--- local/factory.php
+++ local/factory.php
@@ -10,13 +10,30 @@ if (!class_exists('ZtonepageFactory'))
 
     class ZtonepageFactory
     {
-        public static function isLogged () {
+
+        public static function isLogged()
+        {
+            return ZtHelperJoomlaUser::isLogged();
+        }
+
+        public static function isCartPage()
+        {
             
         }
-        
-        public static function isCartPage () {
-            
+
+        public static function html()
+        {
+            if (self::isLogged())
+            {
+                $html = new ZtHtml();
+                return $html->fetch('Ztonepage://html/logged.php');
+            } else
+            {
+                $html = new ZtHtml();
+                return $html->fetch('Ztonepage://html/guest.php');
+            }
         }
+
     }
 
 }
