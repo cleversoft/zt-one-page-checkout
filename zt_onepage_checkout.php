eb8793b6348ed2510e805f5fcaf58a56ef3dc770
Viet Vu <jooservices@gmail.com>
2015-05-26 09:34:12 +0700

    ^ allow load BS3 if love it

--- zt_onepage_checkout.php
+++ zt_onepage_checkout.php
@@ -22,21 +22,30 @@ if (!class_exists('plgSystemZt_onepage_checkout'))
          * @link https://docs.joomla.org/Plugin/Events/System#onAfterRoute
          */
         public function onAfterRoute()
-        {            
+        {
             // Asked for Zt Framework
-            require_once JPATH_ROOT . '/plugins/system/zooframework/core/bootstrap.php';            
+            require_once JPATH_ROOT . '/plugins/system/zooframework/core/bootstrap.php';
             // Register this plugin
             ZtFramework::registerExtension(__DIR__ . '/extension.json');
+            if ($this->params->get('assets_bs3', 0))
+            {
+                ZtAssets::getInstance()->loadVendor('bootstrap', array(
+                    'css/bootstrap.css',
+                    'js/bootstrap.js'
+                ));
+            }
+
             // Check to hook into Virtuemart Cartpage
-            if (ZtonepageHelperVirtuemart::isVirtuemart()) {
-                           
+            if (ZtonepageHelperVirtuemart::isVirtuemart())
+            {
+
                 // Init this plugin
                 $extension = ZtFramework::getExtension('Ztonepage');
                 $extension->init();
                 // Init virtuemart
                 ZtonepageHelperVirtuemart::initVirtueMart();
                 // Override with our custom view
-                ZtonepageHelperVirtuemart::overrideView();             
+                ZtonepageHelperVirtuemart::overrideView();
             }
         }
 
