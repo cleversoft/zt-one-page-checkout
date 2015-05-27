25430413d9a59ed94a3dd2834683aca5bd027478
Viet Vu <jooservices@gmail.com>
2015-05-27 21:46:38 +0700

    ^ update payment form

--- core/virtuemart/views/tmpl/default_payment.php
+++ core/virtuemart/views/tmpl/default_payment.php
@@ -5,7 +5,6 @@
 $modelVM = ZtonepageModelVirtuemart::getInstance();
 $payMentModel = $modelVM->getPayments();
 $class = 'zt-opc-payment';
-
 ?>
 
 <div id="<?php echo $class; ?>-wrap" class="zt-opc-element">
@@ -15,15 +14,28 @@ $class = 'zt-opc-payment';
     </h3>
     <div class="inner-wrap">
         <fieldset>
-            <?php $i = 0;
-            foreach ($payMentModel as $payMent) : ?>
-                <input type="radio" checked="checked" value="<?php echo $i; ?>" id="payment_id_<?php echo $i; ?>"
-                       onclick="" name="virtuemart_paymentmethod_id">
-                <label for="payment_id_<?php echo $i; ?>"><span class="vmpayment"><span
-                            class="vmpayment_name"><?php echo $payMent->payment_name; ?></span></span></label>
+            <?php
+            if ($this->found_payment_method)
+            {
 
-                <div class="clear"></div>
-                <?php $i++; endforeach; ?>
+
+                echo '<fieldset class="vm-payment-shipment-select vm-payment-select">';
+                foreach ($this->paymentplugins_payments as $paymentplugin_payments)
+                {
+                    if (is_array($paymentplugin_payments))
+                    {
+                        foreach ($paymentplugin_payments as $paymentplugin_payment)
+                        {
+                            echo '<div class="vm-payment-plugin-single">' . $paymentplugin_payment . '</div>';
+                        }
+                    }
+                }
+                echo '</fieldset>';
+            } else
+            {
+                echo '<h1>' . $this->payment_not_found_text . '</h1>';
+            }
+            ?>
         </fieldset>
     </div>
 </div>
