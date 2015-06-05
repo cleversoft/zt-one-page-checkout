b041e9393e32b605f194466a5ff0e47b7de2806d
Dung Tran <tad88.dev@gmail.com>
2015-06-05 14:18:56 +0700

    + Add term of service

--- core/virtuemart/views/tmpl/default_confirmpurchase.php
+++ core/virtuemart/views/tmpl/default_confirmpurchase.php
@@ -22,11 +22,21 @@ $class = 'zt-opc-purchase';
             <div class="cart-tos-group">
                 <label class="checkbox <?php echo $class; ?>-tos-label <?php echo $class; ?>-row" for="tos">
                     <input type="checkbox" value="1" name="tos" class="terms-of-service"><?php echo ZtonepageHelperText::_('AGREE'); ?>
-                    <div class="terms-of-service-cont">
-                        <a data-tos="fancybox" class="terms-of-service" href="#<?php echo $class; ?>-tos-fancy"><?php echo ZtonepageHelperText::_('CLICK_HERE_TO_READ_TOS'); ?></a>
+                    <div class="terms-of-service">
+                            <label for="tos">
+                                    <a href="<?php echo JRoute::_ ('index.php?option=com_virtuemart&view=vendor&layout=tos&virtuemart_vendor_id=1', FALSE) ?>" class="terms-of-service" id="terms-of-service" rel="facebox"
+                                       target="_blank">
+                                            <span class="vmicon vm2-termsofservice-icon"></span>
+                                            <?php echo vmText::_ ('COM_VIRTUEMART_CART_TOS_READ_AND_ACCEPTED') ?>
+                                    </a>
+                            </label>
+
+                            <div id="full-tos">
+                                    <h2><?php echo vmText::_ ('COM_VIRTUEMART_CART_TOS') ?></h2>
+                                    <?php echo $cart->vendor->vendor_terms_of_service ?>
+                                    </div>
                     </div>
                 </label>
-
             </div>
             <div class="<?php echo $class; ?>-row <?php echo $class; ?>-checkout-box">
                 <button 
